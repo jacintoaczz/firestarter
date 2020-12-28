5263b2c593292eb71cc56f28b786dabea1e47746
jacintoaczz <jacintoaczz@users.noreply.github.com>
2020-12-28 15:49:58 -0400

    Created a module to share components throught the app. This to keep things organized.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatButton } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,7 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButton,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,37 @@
+// Module where we keep all the components that will be shared throught many places in the app.
+import { NgModule } from '@angular/core';
+
+// Material design components
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+// import { LayoutModule } from '@angular/cdk/layout;
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ShellComponent } from './components/shell/shell.component';
+
+const components = [ShellComponent];
+const modules = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatMenuModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSnackBarModule,
+];
+
+@NgModule({
+  declarations: [...components],
+  imports: [...modules],
+  exports: [...components, ...modules],
+})
+export class SharedModule {}
