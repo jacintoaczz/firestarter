2d0a6b121ac46663d464a87b6f0dd1f99e7660a2
jacintoaczz <jacintoaczz@users.noreply.github.com>
2020-12-28 14:11:45 -0400

    Added custom material theme colors and font.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatButton } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -12,7 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButton,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
