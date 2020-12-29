43c13881ae59eb8022fd6e8f7ef47aef9ff389f6
jacintoaczz <jacintoaczz@users.noreply.github.com>
2020-12-28 23:02:14 -0400

    Defined the app's navigation look and behaviour.

--- src/app/shared/components/shell/shell.component.ts
+++ src/app/shared/components/shell/shell.component.ts
@@ -1,5 +1,8 @@
 // Navigation shell for the entire the app
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-shell',
@@ -7,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shell.component.scss'],
 })
 export class ShellComponent implements OnInit {
-  constructor() {}
+  isHandset$: Observable<boolean> = this.breakPointObserver
+    .observe([Breakpoints.Handset])
+    .pipe(
+      map((result) => result.matches),
+      // Used to subscribe to the observable multiple times
+      shareReplay()
+    );
+
+  // With this we can check in the code for our chosen breakpoints when the screen is resized
+  constructor(private breakPointObserver: BreakpointObserver) {}
 
   ngOnInit(): void {}
 }
