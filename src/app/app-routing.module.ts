177232faeb31f35e8550bc2ba8e400b0d5424859
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-24 00:12:15 -0400

    - Added the '/login' route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
     path: '',
     component: HomePageComponent,
   },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./user/user.module').then((module) => module.UserModule),
+  },
 ];
 
 @NgModule({
