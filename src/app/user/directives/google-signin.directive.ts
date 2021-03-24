02827f4bc45a73647a7094c503c3645cc53ef875
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-24 08:04:52 -0400

    - Code cleanup.

--- src/app/user/directives/google-signin.directive.ts
+++ src/app/user/directives/google-signin.directive.ts
@@ -8,6 +8,10 @@ import firebase from 'firebase/app';
 export class GoogleSigninDirective {
   constructor(private _afAuth: AngularFireAuth) {}
 
+  /* 
+    We attach the onClick  event listener to any element that uses the directive, and handle the
+    login appropiatedly.
+  */
   @HostListener('click')
   onClick() {
     firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
