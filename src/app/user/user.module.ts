36f50d9ea99d0641e9df3cc5cee706678b33b11d
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-24 09:37:48 -0400

    - New component import. - Code cleanup.

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,9 +5,15 @@ import { UserRoutingModule } from './user-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { GoogleSigninDirective } from './directives/google-signin.directive';
 import { SharedModule } from '@app/shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EmailLoginComponent } from './components/email-login/email-login.component';
 
-const components = [LoginPageComponent, GoogleSigninDirective];
-const modules = [UserRoutingModule, SharedModule];
+const components = [
+  EmailLoginComponent,
+  LoginPageComponent,
+  GoogleSigninDirective,
+];
+const modules = [UserRoutingModule, ReactiveFormsModule, SharedModule];
 @NgModule({
   declarations: [...components],
   imports: [...modules],
