e9e79b2fa92811d0be97abe38f7599a39c8c3809
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-29 14:41:31 -0400

    - Added the needed functionality, markup and styling.

--- src/app/features/kanban/components/board-list/board-list.component.ts
+++ src/app/features/kanban/components/board-list/board-list.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Subscription } from 'rxjs';
+import { Board } from '../../board';
+import { BoardService } from '../../services/board.service';
 
 @Component({
   selector: 'app-board-list',
   templateUrl: './board-list.component.html',
-  styleUrls: ['./board-list.component.scss']
+  styleUrls: ['./board-list.component.scss'],
 })
-export class BoardListComponent implements OnInit {
+export class BoardListComponent implements OnInit, OnDestroy {
+  boards: Board[] = [];
+  boardSubscription!: Subscription;
 
-  constructor() { }
+  constructor(public _boardService: BoardService) {}
 
   ngOnInit(): void {
+    this.boardSubscription = this._boardService
+      .getUserBoards()
+      .subscribe((boards: Board[]) => (this.boards = boards));
   }
 
+  ngOnDestroy(): void {
+    // To avoid data leaks and database usage.
+    this.boardSubscription.unsubscribe();
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.boards, event.previousIndex, event.currentIndex);
+    this._boardService.sortBoards(this.boards);
+  }
 }
