7cdd407103ffa22a7025f72c482c18bdf70d02e4
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-29 14:40:29 -0400

    - Created the feature module. - Added all the necessary imports.

--- src/app/features/kanban/kanban.module.ts
+++ src/app/features/kanban/kanban.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { SharedModule } from '@shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { KanbanRoutingModule } from './kanban-routing.module';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BoardComponent } from './components/board/board.component';
+import { BoardListComponent } from './components/board-list/board-list.component';
 
+const components = [BoardComponent, BoardListComponent];
+const modules = [
+  DragDropModule,
+  FormsModule,
+  KanbanRoutingModule,
+  MatButtonToggleModule,
+  MatDialogModule,
+  SharedModule,
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    KanbanRoutingModule
-  ]
+  declarations: [...components],
+  imports: [...modules],
 })
-export class KanbanModule { }
+export class KanbanModule {}
