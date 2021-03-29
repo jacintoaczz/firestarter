100328f676cbec5ca61ddce919bbbf57160f1232
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-29 14:41:19 -0400

    - Added the needed functionality, markup and styling.

--- src/app/features/kanban/components/board/board.component.ts
+++ src/app/features/kanban/components/board/board.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Board } from '../../board';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { BoardService } from '../../services/board.service';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
+  @Input() board!: Board;
 
-  constructor() { }
+  constructor(private _boardService: BoardService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  taskDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.board.tasks, event.previousIndex, event.currentIndex);
+    this._boardService.updateTask(this.board.id, this.board.tasks);
   }
-
 }
