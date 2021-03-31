7424938bfab640d5a30a7ab9e228920cdb0275e3
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-31 15:22:29 -0400

    - Added methods for deleting, updating and addig new tasks.

--- src/app/features/kanban/components/board/board.component.ts
+++ src/app/features/kanban/components/board/board.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Board } from '../../board';
+import { Board, Task } from '../../board';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { MatDialog } from '@angular/material/dialog';
 import { BoardService } from '../../services/board.service';
+import { TaskDialogComponent } from '../../dialogs/task-dialog.component';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
@@ -10,12 +12,41 @@ import { BoardService } from '../../services/board.service';
 export class BoardComponent implements OnInit {
   @Input() board!: Board;
 
-  constructor(private _boardService: BoardService) {}
+  constructor(private _boardService: BoardService, private dialog: MatDialog) {}
 
   ngOnInit(): void {}
 
   taskDrop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.board.tasks, event.previousIndex, event.currentIndex);
     this._boardService.updateTask(this.board.id, this.board.tasks);
+  }
+
+  openDialog(task?: Task, idx?: number): void {
+    const newTask = { label: 'purple' };
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '500px',
+      data: task
+        ? { task: { ...task }, isNew: false, boardId: this.board.id, idx }
+        : { task: newTask, isNew: true },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        if (result.isNew) {
+          this._boardService.updateTask(this.board.id, [
+            ...this.board.tasks,
+            result.task,
+          ]);
+        } else {
+          const update = this.board.tasks;
+          update.splice(result.idx, 1, result.task);
+          this._boardService.updateTask(this.board.id, this.board.tasks);
+        }
+      }
+    });
+  }
+
+  handleDelete() {
+    this._boardService.deleteBoard(this.board.id);
   }
 }
