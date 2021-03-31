a4ab2c6abc5516c99a123b401bc3b0fd7103c3f8
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-31 15:26:49 -0400

    - Added dialog trigger functionality.

--- src/app/features/kanban/components/board-list/board-list.component.ts
+++ src/app/features/kanban/components/board-list/board-list.component.ts
@@ -3,6 +3,8 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Subscription } from 'rxjs';
 import { Board } from '../../board';
 import { BoardService } from '../../services/board.service';
+import { MatDialog } from '@angular/material/dialog';
+import { BoardDialogComponent } from '../../dialogs/board-dialog.component';
 
 @Component({
   selector: 'app-board-list',
@@ -13,7 +15,7 @@ export class BoardListComponent implements OnInit, OnDestroy {
   boards: Board[] = [];
   boardSubscription!: Subscription;
 
-  constructor(public _boardService: BoardService) {}
+  constructor(public _boardService: BoardService, public dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.boardSubscription = this._boardService
@@ -29,5 +31,28 @@ export class BoardListComponent implements OnInit, OnDestroy {
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.boards, event.previousIndex, event.currentIndex);
     this._boardService.sortBoards(this.boards);
+  }
+
+  openBoardDialog(): void {
+    const dialogRef = this.dialog.open(BoardDialogComponent, {
+      width: '400px',
+      data: {},
+    });
+
+    dialogRef.afterClosed().subscribe((result: string) => {
+      if (result) {
+        let newBoard: Board = {
+          title: result,
+          priority: this.boards.length,
+          tasks: [
+            {
+              label: 'purple',
+              description: 'New task!',
+            },
+          ],
+        };
+        this._boardService.createBoard(newBoard);
+      }
+    });
   }
 }
